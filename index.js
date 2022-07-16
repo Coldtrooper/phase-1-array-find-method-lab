e2ba920f6355fc68c81ff5ce602bbb58de5a0d88
Sylvester <sylvestertrevi@gmail.com>
2022-07-16 16:12:15 -0400

    commit tree 6de3629738f384338f83a0086115bb31cb7c23a2

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(record){
+    let found = record.find(champion => champion.result === "W")
+//    if (!!found){
+    // return found.year}
+   return !!found ? found.year : undefined
+}
